5809479479c34791bc7c3d29167d0be18274cf9a
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-08 00:49:22 -0400

    Update Parser.hpp

--- Parser.hpp
+++ Parser.hpp
@@ -1,21 +1,45 @@
 #pragma once
 #include "Token.hpp"
 #include "AST.hpp"
+#include <vector>
 
 class Parser {
 public:
     explicit Parser(const std::vector<Token>& tokens);
-    std::shared_ptr<ASTNode> parse();
+    ASTPtr parseProgram();
 
 private:
-    std::vector<Token> tokens;
-    size_t pos = 0;
+    const std::vector<Token> ts;
+    size_t i=0;
 
-    Token peek() const;
-    Token advance();
-    bool match(TokenType type);
-    Token expect(TokenType type, const std::string& msg);
+    const Token& peek() const;
+    const Token& prev() const;
+    bool check(TokenType t) const;
+    bool match(std::initializer_list<TokenType> set);
+    const Token& expect(TokenType t, const char* msg);
 
-    std::shared_ptr<ASTNode> capsule();
-    std::shared_ptr<ASTNode> statement();
+    // top-level
+    ASTPtr program();
+    ASTPtr capsule();
+    ASTPtr func();
+
+    // common
+    ASTPtr block();             // ':' ... 'end'
+    ASTPtr statement();
+    ASTPtr letDecl();
+    ASTPtr returnStmt();
+    ASTPtr ifStmt();
+    ASTPtr loopStmt();
+    ASTPtr sayStmt();
+    ASTPtr assignOrExprStmt();
+
+    // expressions
+    ASTPtr expression();
+    ASTPtr parsePrecedence(int minPrec);
+    ASTPtr primary();
+    ASTPtr callPostfix(ASTPtr callee);
+
+    int precedenceOf(const Token& t) const;  // *,/ > +,- > compares > && > ||
+    bool isBinaryOp(TokenType t) const;
+    bool isUnaryPrefix(TokenType t) const;
 };
